f85dea88135cca70372c1966792d86dc5960279c
Vichiuri <chiuri.vic@gmail.com>
2025-05-26 16:37:59 +0300

    added sns.se

--- titanic_model.py
+++ titanic_model.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+sns.set(style="whitegrid")
 
 # Load dataset
 df = sns.load_dataset('titanic')
